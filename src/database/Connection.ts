9ed3254226c094f210f6a44da799c84d3a1d8789
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-11-13 13:56:04 -0300

    Database connection

---
+++ src/database/Connection.ts
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+import { config } from 'dotenv';
+config();
+
+const connectDatabase = async () => {
+
+    
+    if(mongoose.STATES[mongoose.connection.readyState] !== "connected")  // if already connected dont make another connection
+        mongoose.connect(process.env.MONGO_URL as string);
+};
+
+export default connectDatabase;
