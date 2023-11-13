3c98fadd9ed8517ff69f4e569f2296e785593d5c
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-11-13 20:05:48 -0300

    TransactionController creation

---
+++ src/database/controllers/TransactionController.ts
@@ -0,0 +1,30 @@
+import { Transaction, ITransaction } from '../models/Transaction';
+
+class TransactionController {
+  async createTransaction(newTransaction: ITransaction): Promise<ITransaction> {
+    try {
+
+      const transaction = await Transaction.create(newTransaction);
+      return transaction;
+    
+    } catch (error) {
+    console.log(error);
+      return null;
+    }
+  }
+
+  async getAllTransactions(userId: string): Promise<ITransaction[]> {
+    try {
+
+      const transactions = await Transaction.find({ "$or": [{ from: userId}, { to: userId }]});
+      return transactions;
+
+    } catch (error) {
+      return [];
+    }
+
+  }
+
+}
+
+export default new TransactionController();
