c707aac183d85ced9c3a3f2a8c1c4ec34c29612f
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-11-13 20:06:24 -0300

    daily command add

--- src/handlers/InteractionHandler.ts
+++ src/handlers/InteractionHandler.ts
@@ -2,11 +2,13 @@ import { CacheType, CommandInteraction, Interaction } from "discord.js";
 import Command from "../api/commands/Command";
 import EditUser from "../api/commands/EditUser";
 import Say from "../api/commands/Say";
+import Daily from "../api/commands/Daily";
 
 
 export const commands: { [key: string]: typeof Command } = {
     "edituser": EditUser,
     "say": Say,
+    "daily": Daily
 }
 
 export default function executeAction(cmdName: string, interaction: Interaction<CacheType>) {
