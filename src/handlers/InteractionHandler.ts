f4d8b0a85ac9c840a96cd9c63ca91ba1f6bc1357
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-11-13 15:59:23 -0300

    edituser command handling

--- src/handlers/InteractionHandler.ts
+++ src/handlers/InteractionHandler.ts
@@ -1,9 +1,10 @@
 import { CacheType, CommandInteraction, Interaction } from "discord.js";
 import Command from "../api/commands/Command";
+import EditUser from "../api/commands/EditUser";
 
 
 export const commands: { [key: string]: typeof Command } = {
-
+    "edituser": EditUser
 }
 
 export default function executeAction(cmdName: string, interaction: Interaction<CacheType>) {
