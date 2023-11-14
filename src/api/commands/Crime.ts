b24c919fd4d001f43e6f32f0ea52e50174c15b46
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-11-14 20:41:20 -0300

    crime chances

--- src/api/commands/Crime.ts
+++ src/api/commands/Crime.ts
@@ -16,7 +16,7 @@ export default class Crime extends Command {
         let min = 750;
         let max = 2000;
         let prob = 0.3;
-        
+
         let user = await UserController.getUserById(interaction.user.id)
         let embed: EmbedBuilder = null;
 
@@ -28,6 +28,7 @@ export default class Crime extends Command {
             min = 3500;
             max = 6000;
             prob = 0.9;
+            
         }
 
         let cash = 0;
@@ -39,30 +40,52 @@ export default class Crime extends Command {
 
 
         let crimeCheck = cooldownCheck(1, user.crimedate, false)
-
+        let canWin = true;
         if (!crimeCheck.allowed) {
             return await interaction.reply({ content: `**${botConfig.WAITING} |** <@${user.userId}>, volte em <t:${crimeCheck.time}> para prender outro criminoso novamente.`});
         }
+            if(!isVipExpired(user) && Math.random() <= botConfig.vipBetChances) {
+                canWin = true;
+            } else if (Math.random() <= botConfig.normalBetChances) {
+                canWin = true;
+            } else {
+                canWin = false;
+            }
+            
+            if(canWin) {
 
-            user.coins = user.coins as number + cash;
-            user.crimedate = new Date();
-
-            user = await UserController.updateUser(user.userId as string, user);
-
-            if (user) {
+                user.coins = user.coins as number + cash;
+                user.crimedate = new Date();
+                user = await UserController.updateUser(user.userId as string, user);
                 let transaction = await TransactionController.createTransaction({
                     to: user.userId as String,
                     from: "prendendo bandidos",
                     ammount: cash,
                 });
-
+                
                 embed = new EmbedBuilder().setTitle(`${botConfig.GG} Crime Impedido`)
                     .setThumbnail(botConfig.IMG_GUN)
                     .setDescription(`> **Perfeito** <@${transaction.to}>, você conseguiu prender um criminoso sem ninguém se machucar e ganhou ${botConfig.getCashString(cash)} como recompensa, volte em **1 hora**.`)
                     .setColor(Colors.White).setTimestamp(Date.now())
                 return await interaction.reply({ embeds: [embed] });
+
             }
-        
+            if(!canWin) {
+                user.coins = user.coins as number - cash;
+                user.crimedate = new Date();
+                user = await UserController.updateUser(user.userId as string, user);
+                let transaction = await TransactionController.createTransaction({
+                    to: "para a cadeia da PF",
+                    from: user.userId,
+                    ammount: cash,
+                });
+                
+                embed = new EmbedBuilder().setTitle(`O Crime Falhou!`)
+                    .setThumbnail(botConfig.IMG_INDIGNATED)
+                    .setDescription(`> **${botConfig.FACEPALM}** <@${user.userId}>, você bateu numa senhora ao invés de bater no ladrão e teve que usar ${botConfig.getCashString(cash)} para pagar o médico, volte em **1 hora**.`)
+                    .setColor(Colors.White).setTimestamp(Date.now())
+                return await interaction.reply({ embeds: [embed] });
+            }
 
 
     }
