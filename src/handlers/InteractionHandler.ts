4335b14913d17ec8fc95dcf5eef789ad70fb7d5e
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-11-13 21:47:47 -0300

    commands handling

--- src/handlers/InteractionHandler.ts
+++ src/handlers/InteractionHandler.ts
@@ -3,12 +3,16 @@ import Command from "../api/commands/Command";
 import EditUser from "../api/commands/EditUser";
 import Say from "../api/commands/Say";
 import Daily from "../api/commands/Daily";
+import Crime from "../api/commands/Crime";
+import Work from "../api/commands/Work";
 
 
 export const commands: { [key: string]: typeof Command } = {
     "edituser": EditUser,
     "say": Say,
-    "daily": Daily
+    "daily": Daily,
+    "crime": Crime,
+    "work": Work,
 }
 
 export default function executeAction(cmdName: string, interaction: Interaction<CacheType>) {
