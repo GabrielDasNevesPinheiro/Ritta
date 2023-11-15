65816f6f736420ba51023613ffc5be5005b89565
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-11-15 15:19:38 -0300

    new assets delivering

--- src/util/ImageUtils.ts
+++ src/util/ImageUtils.ts
@@ -9,25 +9,10 @@ export async function getDouble(sorted: string) {
     const canvas = createCanvas(width, height);
     const ctx: CanvasRenderingContext2D = canvas.getContext("2d");
 
-    let redPoint = 61;
-    let blackPoint = 305;
-    let whitePoint = 610;
-
-    const red = await loadImage(botConfig.LOCAL_IMG_DOUBLE_RED);
-    const black = await loadImage(botConfig.LOCAL_IMG_DOUBLE_BLACK);
-    const white = await loadImage(botConfig.LOCAL_IMG_DOUBLE_WHITE);
-
-    ctx.drawImage(red, 0, 0, 250, 250);
-    ctx.drawImage(black, 250, 0, 250, 250);
-    ctx.drawImage(white, 500, 0, 250, 250);
-
-    const coin = await loadImage(botConfig.LOCAL_IMG_DOUBLE_COIN);
-
     let posX = 0;
-    if(sorted === "red") posX = 61;
-    if(sorted === "black") posX = 305;
-    if(sorted === "white") posX = 549; 
-    ctx.drawImage(coin, posX, 61, 128, 128);
+    if(sorted === "red") ctx.drawImage(await loadImage(botConfig.LOCAL_IMG_DOUBLE_RED_RESULT), 0, 0);
+    if(sorted === "black") ctx.drawImage(await loadImage(botConfig.LOCAL_IMG_DOUBLE_BLACK_RESULT), 0, 0);
+    if(sorted === "white") ctx.drawImage(await loadImage(botConfig.LOCAL_IMG_DOUBLE_WHITE_RESULT), 0, 0); 
 
     const buffer = canvas.toBuffer('image/jpeg');
 
