c4db0ca01b4633d3822120f36c3408b2e36308a5
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-11-17 18:43:54 -0300

    basic dice image generation

--- src/util/ImageUtils.ts
+++ src/util/ImageUtils.ts
@@ -10,9 +10,9 @@ export async function getDouble(sorted: string) {
     const ctx: CanvasRenderingContext2D = canvas.getContext("2d");
 
     let posX = 0;
-    if(sorted === "red") ctx.drawImage(await loadImage(botConfig.LOCAL_IMG_DOUBLE_RED_RESULT), 0, 0);
-    if(sorted === "black") ctx.drawImage(await loadImage(botConfig.LOCAL_IMG_DOUBLE_BLACK_RESULT), 0, 0);
-    if(sorted === "white") ctx.drawImage(await loadImage(botConfig.LOCAL_IMG_DOUBLE_WHITE_RESULT), 0, 0); 
+    if (sorted === "red") ctx.drawImage(await loadImage(botConfig.LOCAL_IMG_DOUBLE_RED_RESULT), 0, 0);
+    if (sorted === "black") ctx.drawImage(await loadImage(botConfig.LOCAL_IMG_DOUBLE_BLACK_RESULT), 0, 0);
+    if (sorted === "white") ctx.drawImage(await loadImage(botConfig.LOCAL_IMG_DOUBLE_WHITE_RESULT), 0, 0);
 
     const buffer = canvas.toBuffer('image/jpeg');
 
@@ -37,10 +37,10 @@ export async function getTigerResult(array: number[][], win: boolean) {
 
     let canvas = createCanvas(800, 800);
     let ctx: CanvasRenderingContext2D = canvas.getContext("2d");
-    
-    if(win) ctx.drawImage(await loadImage(botConfig.LOCAL_IMG_TIGER_WIN), 0, 0);
-    if(!win) ctx.drawImage(await loadImage(botConfig.LOCAL_IMG_TIGER_LOSE), 0, 0);
-    
+
+    if (win) ctx.drawImage(await loadImage(botConfig.LOCAL_IMG_TIGER_WIN), 0, 0);
+    if (!win) ctx.drawImage(await loadImage(botConfig.LOCAL_IMG_TIGER_LOSE), 0, 0);
+
     let assetSize = 128;
 
     let star = await loadImage(botConfig.LOCAL_IMG_TIGER_LOSESTAR)
@@ -49,14 +49,14 @@ export async function getTigerResult(array: number[][], win: boolean) {
 
     let images = [star, cash, tiger];
 
-    
+
 
     let positions: Position[][] = [
-        [{ x: 176, y: 146}, { x: 340 , y: 146}, { x: 504, y: 146}],
-        [ {x: 176, y: 336}, {x: 340, y: 336}, {x: 504, y: 336}],
-        [ {x: 176, y: 530}, {x: 340, y: 530}, {x: 504, y: 530}]
-    ] 
-    
+        [{ x: 176, y: 146 }, { x: 340, y: 146 }, { x: 504, y: 146 }],
+        [{ x: 176, y: 336 }, { x: 340, y: 336 }, { x: 504, y: 336 }],
+        [{ x: 176, y: 530 }, { x: 340, y: 530 }, { x: 504, y: 530 }]
+    ]
+
     ctx.strokeStyle = '#FF0000';
 
     array.forEach((row, rowIndex) => {
@@ -76,25 +76,25 @@ export async function getTigerResult(array: number[][], win: boolean) {
     ctx.beginPath();
     ctx.lineWidth = 10;
 
-    if(horizontal) {
+    if (horizontal) {
         ctx.moveTo(positions[1][0].x + (assetSize / 2), positions[1][0].y + (assetSize / 2));
         ctx.lineTo(positions[1][2].x + (assetSize / 2), positions[1][2].y + (assetSize / 2));
         ctx.stroke();
     }
 
-    if(vertical) {
+    if (vertical) {
         ctx.moveTo(positions[0][1].x + (assetSize / 2), positions[0][1].y + (assetSize / 2));
         ctx.lineTo(positions[2][1].x + (assetSize / 2), positions[2][1].y + (assetSize / 2));
         ctx.stroke();
     }
 
-    if(diagonalLtR) {
+    if (diagonalLtR) {
         ctx.moveTo(positions[0][0].x + (assetSize / 2), positions[0][0].y + (assetSize / 2));
         ctx.lineTo(positions[2][2].x + (assetSize / 2), positions[2][2].y + (assetSize / 2));
         ctx.stroke();
     }
 
-    if(diagonalRtL) {
+    if (diagonalRtL) {
         ctx.moveTo(positions[0][2].x + (assetSize / 2), positions[0][2].y + (assetSize / 2));
         ctx.lineTo(positions[2][0].x + (assetSize / 2), positions[2][0].y + (assetSize / 2));
         ctx.stroke();
@@ -119,13 +119,47 @@ export async function getJackpotResult(array: number[]) {
 
     let icons = [cash, bomb, explosion];
     let positions: Position[] = [
-        { x: 161, y: 89}, {x: 289, y: 89}, { x: 418, y: 89}
+        { x: 161, y: 89 }, { x: 289, y: 89 }, { x: 418, y: 89 }
     ];
 
     array.forEach((item, i) => {
         let img = icons[item]
         ctx.drawImage(img, positions[i].x, positions[i].y, iconSize, iconSize);
     })
+    return canvas.toBuffer();
+
+}
+
+export async function getDice(value: number) {
+    let width = 653;
+    let height = 250;
+
+    const canvas = createCanvas(width, height);
+    const ctx: CanvasRenderingContext2D = canvas.getContext("2d");
+
+    let startPoint: Position = { x: 41, y: 98 }
+    let endPoint: Position = { x: 609, y: 98 }
+
+    
+    const linhaX = startPoint.x + (endPoint.x - startPoint.x) * (value / 100);
+    const linhaY = startPoint.y + (endPoint.y - startPoint.y) * (value / 100);
+
+    ctx.beginPath()
+    ctx.moveTo(startPoint.x, startPoint.y);
+    ctx.lineWidth = 120;
+    ctx.strokeStyle = "#ff0000"; 
+    ctx.lineTo(endPoint.x, endPoint.y);
+    ctx.stroke();
+    
+    ctx.beginPath();
+    ctx.lineWidth = 120;
+    ctx.strokeStyle = "#03fc35"; 
+    ctx.moveTo(startPoint.x, startPoint.y);
+    ctx.lineTo(linhaX, linhaY);
+    ctx.stroke();
+
+    ctx.drawImage(await loadImage(botConfig.LOCAL_IMG_DICE_BACKGROUND), 0, 0);
+
     return canvas.toBuffer();
 
 }
