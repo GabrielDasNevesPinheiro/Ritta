352ee3d54cb2899dcf0b84ad86190130b75b86ca
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-11-16 23:49:18 -0300

    jackpot code

--- src/api/commands/Jackpot.ts
+++ src/api/commands/Jackpot.ts
@@ -1,7 +1,11 @@
 import { CacheType, CommandInteraction, SlashCommandBuilder } from "discord.js";
 import Command from "./Command";
-import { checkBetValues } from "../../util/InteractionUtils";
+import { checkBetValues, getTax } from "../../util/InteractionUtils";
 import { getJackpotResult } from "../../util/ImageUtils";
+import { getJackpotOperation, sortJackpotArray } from "../../util/MatrixUtils";
+import UserController from "../../database/controllers/UserController";
+import { isVipExpired } from "../../util/DateUtils";
+import { botConfig } from "../../app";
 
 
 export default abstract class Jackpot extends Command {
@@ -25,10 +29,54 @@ export default abstract class Jackpot extends Command {
 
         if(!res) return;
 
-        await interaction.editReply({
-            content: `Calma, é só um teste ainda`,
-            files: [await getJackpotResult([])]
-        })
 
+        let jackpot = sortJackpotArray();
+        let result = await getJackpotResult(jackpot);
+        let operation = getJackpotOperation(jackpot);
+        let tax = getTax(ammount);
+
+        let user = await UserController.getUserById(interaction.user.id);
+        if(!isVipExpired(user).allowed) {
+            tax = 0;
+        }
+
+        if(operation === "win") {
+            ammount *= 2;
+            tax = getTax(ammount);
+            ammount -= tax;
+            user = await UserController.addCash(user, {
+                from: "jogando jackpot",
+                to: user.userId,
+                ammount: ammount
+            });
+            
+            return await interaction.editReply({ content: `${botConfig.STONKS} | <@${user.userId}>, Você ganhou ${botConfig.getCashString(ammount)}`+ '`'+`(2x Bônus)`+'`'+` no Jackpot!\n`+
+            (tax > 0 ? `${botConfig.getCashString(tax)} de taxa.` : ``), files: [result]
+        });
+    }
+
+        if(operation === "defeat") {
+            
+            user = await UserController.removeCash(user, {
+                from: user.userId,
+                to: "jogando jackpot",
+                ammount: ammount
+            });
+
+            return await interaction.editReply({ content: `${botConfig.NO_STONKS} | <@${user.userId}>, Você perdeu ${botConfig.getCashString(ammount)} no Jackpot.\n`, files: [result]});
+
+        }
+
+        if(operation === "removePoints") {
+            ammount += getTax(ammount);
+            user = await UserController.removeCash(user, {
+                from: user.userId,
+                to: "jogando jackpot",
+                ammount: ammount
+            });
+
+            return await interaction.editReply({ content: `${botConfig.NO_STONKS} | <@${user.userId}>, Você perdeu ${botConfig.getCashString(ammount)} no Jackpot.\n` + `Pelo resultado **A Casa** leva mais ${botConfig.getCashString(getTax(ammount))}.`, files: [result]});
+
+        }
     }
 }
