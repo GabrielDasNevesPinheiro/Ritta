9adb6d39cb8382c0c5be77612f974d2b4ca0e86f
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-12-08 11:19:20 -0300

    clean code

--- src/handlers/InteractionHandler.ts
+++ src/handlers/InteractionHandler.ts
@@ -1,91 +1,19 @@
 import { CacheType, CommandInteraction, Interaction } from "discord.js";
-import Command from "../api/commands/Command";
-import EditUser from "../api/commands/EditUser";
-import Say from "../api/commands/Say";
-import Daily from "../api/commands/Daily";
-import Crime from "../api/commands/Crime";
-import Work from "../api/commands/Work";
-import Weekly from "../api/commands/Weekly";
-import Atm from "../api/commands/Atm";
-import { Cooldowns } from '../api/commands/Cooldowns';
-import Tasks from '../api/commands/Tasks';
-import Claim from "../api/commands/Claim";
-import Double from "../api/commands/Double";
-import Vote from '../api/commands/Vote';
-import Pay from '../api/commands/Pay';
-import Vip from "../api/commands/Vip";
-import Raffle from '../api/commands/Raffle';
-import Bet from "../api/commands/Bet";
-import { Tiger } from "../api/commands/Tiger";
-import Jackpot from "../api/commands/Jackpot";
-import Drop from "../api/commands/Drop";
-import Mines from "../api/commands/Mines";
-import Dice from "../api/commands/Dice";
-import Horse from "../api/commands/Horse";
-import EditEmoji from "../api/commands/EditEmoji";
-import Transactions from "../api/commands/Transactions";
-import Rep from "../api/commands/Rep";
-import Reps from "../api/commands/Reps";
-import Marry from "../api/commands/Marry";
-import Roulette from "../api/commands/Roulette";
-import Top from "../api/commands/Top";
-import Invite from "../api/commands/Invite";
-import BotInfo from "../api/commands/BotInfo";
+import Command from '../api/commands/Command';
+import path from "path";
+import fs from "fs";
 import UserController from "../database/controllers/UserController";
-import Ban from "../api/commands/Ban";
-import BanList from "../api/commands/BanList";
-import Help from "../api/commands/Help";
-import Scratch from "../api/commands/Scratch";
-import Profile from "../api/commands/Profile";
-import Aboutme from "../api/commands/Aboutme";
-import { cooldowns } from "../app";
-import Inventory from "../api/commands/Inventory";
+import { cooldowns } from "../util/InteractionUtils";
 
+let restricted = ["edituser", "ban", "banlist"];
 
-export const commands: { [key: string]: typeof Command } = {
-    "edituser": EditUser,
-    "daily": Daily,
-    "crime": Crime,
-    "work": Work,
-    "weekly": Weekly,
-    "atm": Atm,
-    "cooldowns": Cooldowns,
-    "tasks": Tasks,
-    "claim": Claim,
-    "double": Double,
-    "vote": Vote,
-    "pay": Pay,
-    "vip": Vip,
-    "raffle": Raffle,
-    "coinflip": Bet,
-    "tiger": Tiger,
-    "jackpot": Jackpot,
-    "drop": Drop,
-    "mines": Mines,
-    "dice": Dice,
-    "horse": Horse,
-    "editemoji": EditEmoji,
-    "transactions": Transactions,
-    "rep": Rep,
-    "reps": Reps,
-    "marry": Marry,
-    "roulette": Roulette,
-    "top": Top,
-    "invite": Invite,
-    "botinfo": BotInfo,
-    "ban": Ban,
-    "banlist": BanList,
-    "help": Help,
-    "scratch": Scratch,
-    "profile": Profile,
-    "aboutme": Aboutme,
-    "inventory": Inventory
-}
+export const commands: { [key: string]: typeof Command } = parseSlashCommands();
+export const restrictedCommands: { [key: string]: typeof Command } = parseRestrictedCommands();
 
 export default function executeAction(cmdName: string, interaction: Interaction<CacheType>) {
     UserController.getUserById(interaction.user.id).then((res) => {
 
-        if(!res?.banned) {
+        if (!res?.banned) {
             commands[cmdName].execute(interaction as CommandInteraction);
             cooldowns.set(interaction.user.id, true);
 
@@ -94,4 +22,58 @@ export default function executeAction(cmdName: string, interaction: Interaction<
             }, 3000);
         }
     });
+}
+
+function parseSlashCommands(): { [key: string]: typeof Command } {
+    const cmdlist: { [key: string]: typeof Command } = {};
+
+
+    const commandsDir = path.join(__dirname, "../api/commands");
+
+    const files = fs.readdirSync(commandsDir);
+
+
+    files.forEach((file) => {
+        if ((file.endsWith(".js") || file.endsWith(".ts"))) {
+
+            if (file.startsWith("Command")) return;
+            
+            const command = require(path.join(commandsDir, file)).default;
+            
+            if (restricted.includes(command?.command.name)) return;
+            
+            if (command?.command && typeof command.execute === "function") {
+                if(command?.command)
+                    cmdlist[command.command.name] = command;
+            }
+        }
+    });
+
+    return cmdlist;
+}
+
+function parseRestrictedCommands(): { [key: string]: typeof Command } {
+    const cmdlist: { [key: string]: typeof Command } = {};
+
+
+    const commandsDir = path.join(__dirname, "../api/commands");
+
+    const files = fs.readdirSync(commandsDir);
+
+
+    files.forEach((file) => {
+        if ((file.endsWith(".js") || file.endsWith(".ts"))) {
+
+            if (file.startsWith("Command")) return;
+
+            const command = require(path.join(commandsDir, file)).default;
+            if (!restricted.includes(command?.command.name)) return;
+            if (command?.command && typeof command.execute === "function") {
+                if(command?.command)
+                    cmdlist[command.command.name] = command;
+            }
+        }
+    });
+
+    return cmdlist;
 }
