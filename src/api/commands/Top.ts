e811ddbe52f7089335bf23ae0327d242ebffb394
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-12-09 14:33:03 -0300

    pagination on new ranking

--- src/api/commands/Top.ts
+++ src/api/commands/Top.ts
@@ -1,4 +1,4 @@
-import { CacheType, CommandInteraction, SlashCommandBuilder, User } from "discord.js";
+import { ActionRowBuilder, ButtonBuilder, ButtonInteraction, ButtonStyle, CacheType, CommandInteraction, ComponentType, SlashCommandBuilder, User } from "discord.js";
 import Command from "./Command";
 import UserController from "../../database/controllers/UserController";
 import getRank from "../../util/ImageUtils";
@@ -7,9 +7,17 @@ import { botConfig } from "../../app";
 export default abstract class Top extends Command {
     
     static command: SlashCommandBuilder = new SlashCommandBuilder()
-        .setName("top").setDescription("Exibe o ranking dos melhores");
+        .setName("top")
+        .addIntegerOption(option =>
+            option.setName("page")
+            .setDescription("Página do ranking")    
+            .setMinValue(1)
+        ).setDescription("Exibe o ranking dos melhores");
 
     static async execute(interaction: CommandInteraction<CacheType>) {
+
+        let page: number = interaction.options.get("page")?.value as number ?? 1;
+        page = page > 0 ? page - 1 : page;
 
         await interaction.deferReply({});
         await interaction.editReply({ content: `${botConfig.WAITING} | <@${interaction.user.id}>, um momento...` });
@@ -21,9 +29,44 @@ export default abstract class Top extends Command {
             fetched.push(await interaction.client.users.fetch(String(user.userId)));
         }
 
-        let ranking = await getRank(fetched, users);
+        let pagesCount = Math.floor(fetched.length / 5);
+        page = page > pagesCount ? pagesCount : page;
 
-        await interaction.editReply({ files: [ranking], content: `` });
+        let ranking = await getRank(fetched, users, page);
+
+        let next = new ButtonBuilder()
+        .setCustomId("next")
+        .setStyle(ButtonStyle.Secondary)
+        .setLabel("Próximo")
+        .setDisabled(page >= pagesCount);
+
+        let prev = new ButtonBuilder()
+        .setCustomId("prev")
+        .setStyle(ButtonStyle.Secondary)
+        .setLabel("Anterior")
+        .setDisabled(page == 0);
+
+        let row = new ActionRowBuilder<ButtonBuilder>().addComponents(prev, next);
+
+        let response = await interaction.editReply({ files: [ranking], content: `🏆 Página ${page + 1} de ${pagesCount + 1} `, components: [row] });
+
+        let collectorFilter = (i: ButtonInteraction) => i.user.id === interaction.user.id;
+        const collector = response.createMessageComponentCollector({ componentType: ComponentType.Button, time: 120000, filter: collectorFilter });
+
+        collector.on("collect", async (confirmation) => {
+
+            if(confirmation.customId === "next") page += 1;
+            if(confirmation.customId === "prev") page -= 1;
+
+            next.setDisabled(page >= pagesCount);
+            prev.setDisabled(page == 0);
+
+            ranking = await getRank(fetched, users, page);
+            
+            await interaction.editReply({ content: `🏆 Página ${page + 1} de ${pagesCount + 1}`, files: [ranking], components: [row] });
+            await confirmation.update({});
+
+        });
 
     }
 }
