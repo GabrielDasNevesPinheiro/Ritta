32fb4de8c3e3b423bbcc0718c47805376410a00e
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-12-09 16:57:09 -0300

    nogb background set

--- src/util/BotConfig.ts
+++ src/util/BotConfig.ts
@@ -108,6 +108,7 @@ export default class BotConfig {
     LOCAL_IMG_RANK = "./src/assets/rank_template.png"
     LOCAL_IMG_ROULETTEVIP = "./src/assets/roleta_vip.png"
     LOCAL_IMG_PROFILE_TEMPLATE = "./src/assets/profile_template.png"
+    LOCAL_IMG_PROFILE_TEMPLATE_NOBG = "./src/assets/profile_template_nobg.png"
     GIF_DOUBLE = "https://i.imgur.com/onMSkcv.gif"
     GIF_TIGER = "https://i.imgur.com/XfWN4e6.gif"
     GIF_2DICE = "https://i.imgur.com/KnnOITl.gif"
