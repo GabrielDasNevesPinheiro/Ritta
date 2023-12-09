337f3bab1eda8ba9f1e7cd811f4c1ac8ca5f2e0e
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-12-09 20:01:28 -0300

    final item store structure

--- src/database/models/Store.ts
+++ src/database/models/Store.ts
@@ -6,18 +6,26 @@ enum ItemType {
     PET = 2
 }
 
+enum Rarity {
+    COMMON = 0,
+    RARE = 1,
+    MYTHIC = 2,
+    ULTRA_MYTHIC = 3,
+}
+
 interface IStore {
     name: string;
     price: number;
     url: string;
+    restricted?: Boolean;
     itemType: number;
+    rarity: number;
 }
 
 
 const storeSchema = new mongoose.Schema<IStore>({
     name: {
         type: String,
-        unique: true,
         required: true
     },
     price: {
@@ -31,10 +39,19 @@ const storeSchema = new mongoose.Schema<IStore>({
     itemType: {
         type: Number,
         required: true,
+    },
+    restricted: {
+        type: Boolean,
+        required: false,
+        default: false
+    },
+    rarity: {
+        type: Number,
+        required: true
     }
 });
 
 const Store: mongoose.Model<IStore> = mongoose.models.Marry || mongoose.model<IStore>("Store", storeSchema);
 
 
-export { Store, type IStore, ItemType }
+export { Store, type IStore, ItemType, Rarity }
