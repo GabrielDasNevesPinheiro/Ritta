bdc04133535538f8f9aafd36996511c0a3cd050c
gabriel <gabrieldasnevespinheiro@gmail.com>
2023-12-23 17:11:29 -0300

    idk what changed

--- src/api/commands/Atm.ts
+++ src/api/commands/Atm.ts
@@ -34,7 +34,7 @@ export default class Atm extends Command {
 
 			if(player.userId === userId) place = index + 1;
 		});
-
+        
         let targetUser = interaction.client.users.cache.get(user.userId as string);
         await interaction.reply({ 
             content: `${botConfig.OMG} | <@${interaction.user.id}>, ${interaction.user.id === user.userId ? "Você" : "`" +`${targetUser.username}`+ "`"} tem ${botConfig.getCashString(points as number)} e está em **#${place}** entre os mais ricos, para ver o ranking, use o comando `+ "`" + '/top' + "`."
